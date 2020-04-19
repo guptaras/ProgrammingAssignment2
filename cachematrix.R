a7906c18c82666387d845c2d3df2bd31455af6a9
guptaras <26242752+guptaras@users.noreply.github.com>
2020-04-20 00:42:51 +0530

    My first Commit To Assignment files

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## x is iniialized as function argument
 makeCacheMatrix <- function(x = matrix()) {
+        s <- NULL  ## make variable to later used
+        ## this function set the values to the parent enviourment
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) s <<- solve(x)
+        getinverse <- function() s
+        ## allows us to use $ form of extract operator
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## this function will check the value of s .If S value is cached before than it will retreive 
+##from cache otherwise will show null nd compute the inverse of matrix nd cache it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+        s <- x$getinverse()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setinverse(s)
+        s}
